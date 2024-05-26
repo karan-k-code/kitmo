b45de6eb1e01f0a91037fee80cd55eba02f24da1
karan kumar <nolimetedmoney4@gmail.com>
2024-05-27 02:43:30 +0530

    fix erro

--- src/dark.js
+++ src/dark.js
@@ -2,8 +2,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const darkModeToggle = document.getElementById('dark-mode-toggle-checkbox');
     darkModeToggle.addEventListener('change', function () {
-        const light = document.getElementsByClassName("light");
+        const lightb = document.getElementsByClassName("light");
         document.body.classList.toggle('dark-mode', darkModeToggle.checked);
-         light.classList.toggle('dark-mode', darkModeToggle.checked);
+        
     });
 });
+
+// let darkbtn = document.querySelector("#darkbtn");
+
+// console.dir(darkbtn);
+
+// let hello =()=>{
+//     console.log("hello");
+// }
+
+//  darkbtn.addEventListener('onclick' ,console.log("hello"));
+
+let lightbtn = document.querySelector(".menu-container");
+
+console.dir(lightbtn);
+
+lightbtn.classList.add('hii');
