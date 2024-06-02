337a72cf765e56d1492635cba07f0083465b6840
karan kumar <nolimetedmoney4@gmail.com>
2024-06-02 09:07:53 +0530

    fix dark mode error

--- src/dark.js
+++ src/dark.js
@@ -1,37 +1,18 @@
-const btn = document.querySelectorAll(".light");
 
+const darkModeToggle = document.getElementById('dark-mode-toggle-checkbox');
+console.log(darkModeToggle);
 
-console.log(btn);
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeToggle = document.getElementById('dark-mode-toggle-checkbox');
-    darkModeToggle.addEventListener('change', function () {
-        // const lightb = document.getElementsByClassName("light");
-        document.body.classList.toggle('dark-mode', darkModeToggle.checked);
-        // btn.classList.replace("no_dark", "dark-mode");
-        btn.classList.toggle('dark-mode', darkModeToggle.checked);
-
-        
+darkModeToggle.addEventListener("change", function() {
+    let buttons = document.querySelectorAll(".light");
+    // Iterate over each button and add the 'dark-mode' class
+    buttons.forEach(function(btn) {
+        if (darkModeToggle.checked) {
+            
+            btn.classList.add("dark-mode");
+        } else {
+            btn.classList.remove("dark-mode");
+        }
     });
+    
+    
 });
-
-// let darkbtn = document.querySelector("#darkbtn");
-
-// console.dir(darkbtn);
-
-// let hello =()=>{
-//     console.log("hello");
-// }
-
-//  darkbtn.addEventListener('onclick' ,console.log("hello"));
-
-// let btn = document.querySelector(".light")
-
-// let lightbtn = document.querySelector("#darkbtn");
-
-// lightbtn.onclick = (e)=>{
-//     console.log(e);
-//     btn.classList.add('dark-mode');
-//     btn.classList.remove('light');
-// }
