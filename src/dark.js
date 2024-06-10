4168d3729119a423b7fdbac65f151ef97a68611f
karan kumar <nolimetedmoney4@gmail.com>
2024-06-11 01:43:31 +0530

    add dark mode hover

--- src/dark.js
+++ src/dark.js
@@ -1,11 +1,10 @@
 
 const darkModeToggle = document.getElementById('dark-mode-toggle-checkbox');
-console.log(darkModeToggle);
 
 darkModeToggle.addEventListener("change", function() {
     let buttons = document.querySelectorAll(".light");
     let nav_bar = document.querySelectorAll(".dark_box");
-    let option_dark = document.querySelectorAll(".option_h");
+    let opt_dark = document.querySelectorAll(".option_b");
     // Iterate over each button and add the 'dark-mode' class
     buttons.forEach(function(btn) {
         if (darkModeToggle.checked) {
@@ -25,15 +24,14 @@ darkModeToggle.addEventListener("change", function() {
             btn.classList.add("nav_light");
         }
     });
-    option_dark.forEach(function(btn) {
+    opt_dark.forEach(function(btn) {
         if (darkModeToggle.checked) {
             btn.classList.remove("option_h");
             btn.classList.add("dark_option_h");
          } else {
             btn.classList.remove("dark_option_h");
             btn.classList.add("option_h");
-            console.log("hello");
         }
     });
-        
+
 });
