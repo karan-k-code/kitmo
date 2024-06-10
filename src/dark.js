682167d4b76553362245c9aa084ddce4ed428a5a
karan kumar <nolimetedmoney4@gmail.com>
2024-06-10 09:43:25 +0530

    add dark mode to cart page

--- src/dark.js
+++ src/dark.js
@@ -5,13 +5,15 @@ console.log(darkModeToggle);
 darkModeToggle.addEventListener("change", function() {
     let buttons = document.querySelectorAll(".light");
     let nav_bar = document.querySelectorAll(".dark_box");
+    let option_dark = document.querySelectorAll(".option_h");
     // Iterate over each button and add the 'dark-mode' class
     buttons.forEach(function(btn) {
         if (darkModeToggle.checked) {
-            
             btn.classList.add("dark-mode");
+            btn.classList.remove("no_dark")
         } else {
             btn.classList.remove("dark-mode");
+            btn.classList.add("no_dark")
         }
     });
     nav_bar.forEach(function(btn) {
@@ -23,6 +25,15 @@ darkModeToggle.addEventListener("change", function() {
             btn.classList.add("nav_light");
         }
     });
-    
-    
+    option_dark.forEach(function(btn) {
+        if (darkModeToggle.checked) {
+            btn.classList.remove("option_h");
+            btn.classList.add("dark_option_h");
+         } else {
+            btn.classList.remove("dark_option_h");
+            btn.classList.add("option_h");
+            console.log("hello");
+        }
+    });
+        
 });
