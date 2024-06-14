78136c623c5dd9e9597c24257059459fe08fb1bd
karan kumar <nolimetedmoney4@gmail.com>
2024-06-14 14:49:53 +0530

    fix error

--- src/cart.js
+++ src/cart.js
@@ -2,11 +2,6 @@
 // let headphone = document.getElementById("headphone");
 // console.log(headphone);
 import{shopItamsData} from './shopdata.js';
-for(let i of shopItamsData){
-    // console.log(i);
-}
-// for(let i =0; i<shopItamsData.length; i++){
-//     console.log(i);
-//     console.log(shopItamsData[i]);
-// }
-// console.log(shopItamsData.length);
+console.log(shopItamsData);
+
+ 
