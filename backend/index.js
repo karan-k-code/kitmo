927135b46306663ca1d9e89b8cc374ac0015d434
karan kumar <nolimetedmoney4@gmail.com>
2024-07-22 07:49:51 +0530

    add some files

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
