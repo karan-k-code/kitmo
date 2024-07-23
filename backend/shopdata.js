a124360a8450c961e82ed63d02b5a205dba22205
karan kumar <nolimetedmoney4@gmail.com>
2024-07-23 16:30:13 +0530

    no chenge

---
+++ backend/shopdata.js
@@ -0,0 +1,63 @@
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+let shopItamsData = [
+  {
+    id: "headphones",
+    name: "headphones",
+    price: 22,
+    desc: "this is boat headphones",
+    img: "image/headphone.jpeg",
+  },
+  {
+    id: "goggles",
+    name: "goggles",
+    price: 9,
+    desc: "stylish goggles new year",
+    img: "image/goggles.jpeg",
+  },
+  {
+    id: "short",
+    name: "short",
+    price: 25,
+    desc: "stylish short for man",
+    img: "image/short.jpeg",
+  },
+  {
+    id: "jeans",
+    name: "jeans",
+    price: 30,
+    desc: "boy jeans so prime",
+    img: "image/jeans.jpeg",
+  },
+  {
+    id: "cup",
+    name: "cup",
+    price: 9,
+    desc: "this is stylish cup",
+    img: "image/cup.jpeg",
+  },
+  {
+    id: "iPhone",
+    name: "mobile",
+    price: 999,
+    desc: "iPhone 15 pro max",
+    img: "image/iphone.jpg",
+  },
+];
+// app.use(express.static('public'))
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+// app.get('/about', (req, res) => {
+//   res.send(shopItamsData)
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
