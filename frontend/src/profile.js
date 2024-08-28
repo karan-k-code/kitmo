7f15b470e6dc840fe3edec4e1b11f2dd52f63df6
karan kumar <nolimetedmoney4@gmail.com>
2024-08-28 23:29:45 +0530

    add node.Js

--- frontend/src/profile.js
+++
@@ -1,8 +0,0 @@
-function showMore() {
-    var moreInfo = document.getElementById('more-info');
-    if (moreInfo.style.display === 'none') {
-        moreInfo.style.display = 'block';
-    } else {
-        moreInfo.style.display = 'none';
-    }
-}
