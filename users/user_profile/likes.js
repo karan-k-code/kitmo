ec182427cde8e3ab9fa31411b96c0a532d4c6db5
karan kumar <nolimetedmoney4@gmail.com>
2024-11-30 12:59:19 +0530

    add sidebar border funcation and fix error

---
+++ users/user_profile/likes.js
@@ -0,0 +1,22 @@
+const likes = document.getElementById("likes");
+
+const ganretLikes = async () => {
+  const url = urls + "/likes/all_likes";
+  const response = await apiCallGet(url);
+
+  if (response.data.lenght !== 0) {
+    likes.innerHTML = response.data
+      .map((x) => {
+        const { _id, productName, productPrice, productDescription, image } = x;
+        return `<div class="product box_shadow box_no_dark_shadow">
+          <img src="${image[0].img}" alt="" srcset="" onclick="goo('${_id}')" />
+          <div class="dec">
+        <p>${productDescription}</p>
+          </div>
+        </div>`;
+      })
+      .join("");
+  }
+};
+
+ganretLikes();
---
+++ users/user_profile/sidebar.js
@@ -0,0 +1,71 @@
+const likeBtn = document.getElementById("likebtn");
+const profileBtn = document.getElementById("profile");
+const saveBtn = document.getElementById("saveBtn");
+const orderBtn = document.getElementById("orderBtn");
+const settingBtn = document.getElementById("SettingBtn");
+const histryBtn = document.getElementById("histryBtn");
+
+const profileDetailes = document.getElementById("profile_detailes");
+const likef = document.getElementById("likes");
+
+let click = profileBtn;
+
+const sidebar = document.getElementById("sidebar");
+
+likeBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = likeBtn;
+  currentclick();
+  profileDetailes.style.display = "none";
+  likef.style.display = "grid";
+});
+profileBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = profileBtn;
+  currentclick();
+
+  profileDetailes.style.display = "flex";
+  likef.style.display = "none";
+});
+orderBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = orderBtn;
+  currentclick();
+  profileDetailes.style.display = "flex";
+  likef.style.display = "none";
+});
+saveBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = saveBtn;
+  currentclick();
+
+  profileDetailes.style.display = "flex";
+  likef.style.display = "none";
+});
+settingBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = settingBtn;
+  currentclick();
+
+  profileDetailes.style.display = "flex";
+  likef.style.display = "none";
+});
+histryBtn.addEventListener("click", (x) => {
+  remoreclick();
+  click = histryBtn;
+  currentclick();
+
+  profileDetailes.style.display = "flex";
+  likef.style.display = "none";
+});
+
+const currentclick = () => {
+  click.style.borderColor = "rgb( 78 , 78 , 78)";
+};
+
+const remoreclick = () => {
+  click.style.borderColor = "transparent";
+  //   click.style.borderColor = "transparent";
+};
+
+currentclick();
